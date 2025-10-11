650c34ba4b4503d6a32f6e4b0fdca0fac08950cc
Mateus <garciamateus285@gmail.com>
2025-10-11 09:29:54 -0300

    feat: implementar normalização vetorial e assinatura de funções principais

--- ray_tracing.py
+++ ray_tracing.py
@@ -22,4 +22,28 @@ pixel_size_z = plane_size_z / height  # 4.0
 centro_esfera = np.array([5.0, 0.0, 0.0])
 raio_esfera = 2.0
 
+# função utilitária
+def normalize(v):
+    n = np.linalg.norm(v) # calcula a norma do vetor [sqrt(x**x+y**y+z**z)]
+    return (v/n) if n > 0 else v
 
+
+# funções principais
+
+
+# Interseção raio-esfera
+# (d*d)t^2 + 2(d*(e-c))t + ((e-c)*(e-c)-r^2) = 0
+
+# – Se Δ < 0, não há interseção (pixel = preto).
+# – Se Δ ≥ 0, calcule as raízes e pegue a menor positiva t.
+def intersect_esfera():
+    pass
+
+def intensidade_phong():
+    pass
+
+def renderizar():
+    pass
+
+if __name__ == "__main__":
+    pass
